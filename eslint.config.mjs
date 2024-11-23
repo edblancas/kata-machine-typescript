aa54e7cce10cc577c185ee6191c57c76b7d91337
Daniel Blancas <edblancas@gmail.com>
2024-11-23 10:13:48 -0600

    Fix eslint +9.0.0, .eslintrc.json is not longer used
    See:
    https://eslint.org/docs/latest/use/getting-started#configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("plugin:@typescript-eslint/recommended", "prettier"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+    },
+}];

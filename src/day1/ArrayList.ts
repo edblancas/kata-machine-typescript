5781e3a4b03ce5084bb9478674c18697cbfe804d
Daniel Blancas <edblancas@gmail.com>
2025-01-25 19:07:16 -0600

    wip arraylist

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,61 @@
 export default class ArrayList<T> {
-    public length: number;
+  public length: number = 0
+  private capacity: number;
+  private arr: T[];
 
-    
+  constructor(capacity: number) {
+    this.capacity = capacity
+    this.arr = Array(capacity).fill(0)
+  }
 
-    constructor() {
+  prepend(item: T): void {
+    if (this.length === 0) {
+      this.arr[0] = item
+      return
     }
 
-    prepend(item: T): void {
+    if (this.length + 1 > this.capacity) {
+      let newArr = Array(this.capacity * 2).fill(0)
+      for (let i = 1; i <= this.capacity; i++) {
+        newArr[i] = this.arr[i - 1]
+      }
+      this.arr = newArr
+      this.length = this.capacity + 1
+      this.capacity *= 2
+    } else {
+      let tmp = this.arr[0]
+      let tmp2
+      for (let i = 0; i <= this.length; i++) {
+        tmp = this.arr[i]
+        tmp2 = this.arr[i + 1]
+        this.arr[i + 1] = tmp
+        tmp = tmp2
+      }
+      this.arr[0] = item
+    }
+  }
 
-}
-    insertAt(item: T, idx: number): void {
+  insertAt(item: T, idx: number): void {
 
-}
-    append(item: T): void {
+  }
 
-}
-    remove(item: T): T | undefined {
+  append(item: T): void {
+    if (this.length === this.capacity) {
+    }
+    this.arr[this.length] = item
+  }
 
-}
-    get(idx: number): T | undefined {
+  remove(item: T): T | undefined {
+  }
 
-}
-    removeAt(idx: number): T | undefined {
+  get(idx: number): T | undefined {
+    if (idx < 0 || idx >= this.length) return undefined
+    return this.arr[idx]
+  }
 
-}
+  removeAt(idx: number): T | undefined {
+    if (idx < 0 || idx >= this.length) return undefined
+    this.length--
+    return this.arr[this.length + 1]
+  }
 }

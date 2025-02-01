29441bc6172690f2e8c6a09af424b678488e080a
Daniel Blancas <edblancas@gmail.com>
2025-02-01 10:54:39 -0600

    add arraylist

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -11,51 +11,83 @@ export default class ArrayList<T> {
   prepend(item: T): void {
     if (this.length === 0) {
       this.arr[0] = item
+      this.length++
+      console.log('prepend', this.arr, this.length)
       return
     }
 
-    if (this.length + 1 > this.capacity) {
+    if (this.length === this.capacity) {
+      let newArr = Array(this.capacity * 2).fill(0)
+      for (let i = 1; i < this.length + 1; i++) {
+        newArr[i] = this.arr[i - 1]
+      }
+      this.arr = newArr
+      this.length++
+      this.capacity *= 2
+    } else {
+      for (let i = this.length; i >= 1; i--) {
+        this.arr[i] = this.arr[i - 1]
+      }
+      this.arr[0] = item
+      this.length++
+    }
+
+    console.log('prepend', this.arr, this.length)
+  }
+
+  insertAt(item: T, idx: number): void {
+    if (this.arr.length + 1 > this.capacity) {
       let newArr = Array(this.capacity * 2).fill(0)
       for (let i = 1; i <= this.capacity; i++) {
         newArr[i] = this.arr[i - 1]
       }
       this.arr = newArr
-      this.length = this.capacity + 1
+      this.length++
       this.capacity *= 2
-    } else {
-      let tmp = this.arr[0]
-      let tmp2
-      for (let i = 0; i <= this.length; i++) {
-        tmp = this.arr[i]
-        tmp2 = this.arr[i + 1]
-        this.arr[i + 1] = tmp
-        tmp = tmp2
-      }
-      this.arr[0] = item
     }
-  }
 
-  insertAt(item: T, idx: number): void {
-
+    for (let i = idx; i < this.length; i++) {
+      this.arr[i + 1] = this.arr[i]
+    }
+    this.arr[idx] = item
   }
 
   append(item: T): void {
     if (this.length === this.capacity) {
+      let newArr = Array(this.capacity * 2).fill(0)
+      for (let i = 0; i < this.length; i++) {
+        newArr[i] = this.arr[i]
+      }
+      this.arr = newArr
     }
     this.arr[this.length] = item
+    this.length++
   }
 
   remove(item: T): T | undefined {
+    let i = 0
+    while (i < this.length && this.arr[i] != item) {
+      i++
+    }
+    if (i === this.length) return undefined
+    this.removeAt(i)
+    return item
   }
 
   get(idx: number): T | undefined {
-    if (idx < 0 || idx >= this.length) return undefined
+    if (idx < 0 || idx >= this.length) {
+      return undefined
+    }
     return this.arr[idx]
   }
 
   removeAt(idx: number): T | undefined {
     if (idx < 0 || idx >= this.length) return undefined
+    const ret = this.arr[idx]
+    for (let i = idx; this.length > i; i++) {
+      this.arr[i] = this.arr[i + 1]
+    }
     this.length--
-    return this.arr[this.length + 1]
+    return ret
   }
 }
